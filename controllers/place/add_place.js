f2166109e88b61cfae5514554105cd1c94700f4b
SasithDuleepa <duleepajayathissa@gmail.com>
2023-10-12 13:21:25 +0530

    form data

--- controllers/place/add_place.js
+++ controllers/place/add_place.js
@@ -2,11 +2,18 @@ const DB = require('./../../config/database');
 
 const AddPlace = (req, res) => {
     console.log(req.body);
-    const {name,description,lat,lng} = req.body;
+    let filename = null;
+    const {name,description,time,lat,lng} = req.body;
+
+    if (req.files && req.files.length > 0) {
+        filename = req.files[0].filename;
+        
+     
+      console.log(filename);
     if(name=='' || description==='' || lat==="" || lng===""){
         res.send({status:400,message:"All fields are required"})
     }else{
-        const query = `INSERT INTO place (place_name,place_description,place_lat,place_lng) VALUES ('${name}','${description}',${lat},${lng})`;
+        const query = `INSERT INTO place (place_name,place_description,place_lat,place_lng,visit_time,place_img) VALUES ('${name}','${description}',${lat},${lng},'${time}','${filename}')`;
         DB.connection.query(query, (err, result) => {
             if (result) {
                 res.send({status:200,message:"Place added successfully"})
@@ -17,6 +24,7 @@ const AddPlace = (req, res) => {
     });
        
     }
+}
 }
 
 
--- routes/place_routes.js
+++ routes/place_routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const  path = require('path');
 
-
+const uploads = require('./../middleware/multer/multer');
 const AddPlace =  require('./../controllers/place/add_place')
 
 
 
 const router = express.Router();
+router.use(express.static(path.join((__dirname, "uploads"))));
 
-router.post('/addplace', AddPlace);
+;
+router.post('/addplace',uploads.array('file'), AddPlace);
 
 module.exports = router;

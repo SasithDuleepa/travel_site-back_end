3dd29e06ae04533006ae4a86cd70ef180b99044c
SasithDuleepa <duleepajayathissa@gmail.com>
2023-10-12 13:29:53 +0530

    get files

--- middleware/multer/place.js
+++ middleware/multer/place.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 //multer config
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null,'./uploads');
+        cb(null,'./uploads/places');
     },
     filename: function(req,file,cb){
         if(file){
--- routes/place_routes.js
+++ routes/place_routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const  path = require('path');
 
-const uploads = require('./../middleware/multer/multer');
+const uploads = require('../middleware/multer/place');
 const AddPlace =  require('./../controllers/place/add_place')
+const Placeimg = require('./../controllers/place/place_img')
 
 
 
 const router = express.Router();
-router.use(express.static(path.join((__dirname, "uploads"))));
+router.use(express.static(path.join((__dirname, "uploads/places"))));
 
-;
+router.get('/placeimg', Placeimg);
 router.post('/addplace',uploads.array('file'), AddPlace);
 
 module.exports = router;
